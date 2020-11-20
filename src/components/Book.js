3d7b5ab4826048752401fe7ead28ebcbbb46375c
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-20 16:04:03 -0700

    Connect the REMOVE_BOOK action to the component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Book({ data }) {
+export default function Book({ data, removeBook }) {
   const { bookId, title, category } = data;
 
   return (
@@ -9,10 +9,23 @@ export default function Book({ data }) {
       <td>{bookId}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td>
+        <button
+          type="button"
+          onClick={() => removeBook(data)}
+        >
+          Remove Book
+        </button>
+      </td>
     </tr>
   );
 }
 
 Book.propTypes = {
-  data: PropTypes.objectOf(PropTypes.object).isRequired,
+  data: PropTypes.shape({
+    bookId: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,14 +1,16 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions';
 
+const bookIds = () => Math.ceil(Math.random() * 100);
+
 function bookReducer(state = [], action) {
   switch (action.type) {
     case CREATE_BOOK:
       return [
         ...state,
         {
-          bookId: action.bookId,
-          title: action.title,
-          category: action.category,
+          bookId: bookIds(),
+          title: action.book.title,
+          category: action.book.category,
         },
       ];
     case REMOVE_BOOK:
