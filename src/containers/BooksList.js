ec40c917022a57fbae2bc9979ab86f01cf52f7af
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-20 16:03:53 -0700

    Connect the REMOVE_BOOK action to the BooksList

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,8 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
+import { removeBook } from '../actions';
 
-function BooksList({ books }) {
+function BooksList({ books, removeBook }) {
+  const handleRemoveBook = (book => removeBook(book));
+
   return (
     <table>
       <thead>
@@ -14,18 +17,31 @@ function BooksList({ books }) {
         </tr>
       </thead>
       <tbody>
-        {books.map(book => <Book key={book.bookId} data={book} />)}
+        {books.map(book => <Book key={book.bookId} data={book} removeBook={handleRemoveBook} />)}
       </tbody>
     </table>
   );
 }
 
 BooksList.propTypes = {
-  books: PropTypes.arrayOf(PropTypes.object).isRequired,
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      bookId: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   books: state.books,
 });
 
-export default connect(mapStateToProps, null)(BooksList);
+const mapDispatchToProps = dispatch => ({
+  removeBook: bookToRemove => {
+    dispatch(removeBook(bookToRemove));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
