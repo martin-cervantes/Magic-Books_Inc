5146fa811e376f96d5e6d3525734d0ec5a74a5dd
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-20 18:10:34 -0700

    Fix objetc inmutablity assign

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -5,16 +5,18 @@ const bookIds = () => Math.ceil(Math.random() * 100);
 function bookReducer(state = [], action) {
   switch (action.type) {
     case CREATE_BOOK:
-      return [
-        ...state,
-        {
-          bookId: bookIds(),
-          title: action.book.title,
-          category: action.book.category,
-        },
-      ];
+    return Object.assign({}, state, { books:[
+      ...state.books,
+      {
+        bookId: bookIds(),
+        title: action.book.title,
+        category: action.book.category,
+      },
+    ]});
     case REMOVE_BOOK:
-      return state.filter(book => book.bookId === action.bookId);
+      return Object.assign({}, state, {
+        books:state.books.filter(book => book.bookId !== action.book.bookId)
+      });
     default:
       return state;
   }
