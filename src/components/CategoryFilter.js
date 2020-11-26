00d8e840ae825f7998f32e10e560523cafb831e2
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-25 18:23:27 -0700

    Add handleFilterChange

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { categories } from '../store/init';
 
-export default function CategoryFilter() {
+export default function CategoryFilter(props) {
   return (
     <div>
-      <select name="filter">
+      <select name="filter" onChange={props.handleFilterChange}>
         <option value="All" key="All">All</option>
         {categories.map(category => (
           <option value={category} key={category}>{category}</option>
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import CategoryFilter from '../components/CategoryFilter';
+import { connect } from 'react-redux';
+import { changeFilter } from '../actions';
 
-export default function Header() {
-  return (
-    <header>
-      <CategoryFilter />
-    </header>
-  );
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+  }
+
+  handleFilterChange(event) {
+    event.preventDefault();
+
+    this.props.filterBooks(event.target.value);
+  }
+
+  render() {
+    return (
+      <header>
+        <CategoryFilter handleFilterChange={this.handleFilterChange} />
+      </header>
+    );
+  }
 }
+const mapDispatchToProps = dispatch => ({
+  filterBooks: filter => {
+    dispatch(changeFilter(filter));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(Header);
