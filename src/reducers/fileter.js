458b2f1e6d49229d30e19ba2c05462f4103622b1
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-27 14:22:41 -0700

    Fix object inmutability assign

--- src/reducers/fileter.js
+++ src/reducers/fileter.js
@@ -3,7 +3,7 @@ import { CHANGE_FILTER } from '../actions';
 function filterReducer(state = {}, action) {
   switch (action.type) {
     case CHANGE_FILTER:
-      return action.category;
+      return Object.assign({}, state, { filter: action.category});
     default:
       return state;
   }
