66784474d35dd19471df9b302e9025b478bd8709
Martin Cervantes <cervantes.martine@gmail.com>
2020-12-02 09:52:04 -0700

    Add onChange to handle Filter Change

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { categories } from '../store/init';
 
-export default function CategoryFilter(props) {
+export default function CategoryFilter({ handleFilterChange }) {
   return (
     <div>
-      <select name="filter" onChange={props.handleFilterChange}>
+      <select name="filter" onChange={(e) => handleFilterChange(e)}>
         <option value="All" key="All">All</option>
         {categories.map(category => (
           <option value={category} key={category}>{category}</option>
